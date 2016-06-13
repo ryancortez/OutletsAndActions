759ddab882e1918f18ed499843f2b2866f39d239
ryancortez <ryancortez@mac.com>
2016-06-13 13:47:51 -0500

    Initial Commit

---
+++ OutletsAndActions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutletsAndActions
+//
+//  Created by Ryan Cortez on 6/13/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
